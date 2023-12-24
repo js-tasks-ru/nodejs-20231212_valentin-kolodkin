aeccee4522d9253fe541c5617470a93258569b6e
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2023-12-24 16:15:28 +0300

    Задача 3. Стрим с лимитом передачи данных

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.bytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.bytes += chunk.length;
 
+    if (this.bytes <= this.limit) {
+      callback(null, chunk);
+    } else {
+      callback(new LimitExceededError());
+    }
   }
 }
 
