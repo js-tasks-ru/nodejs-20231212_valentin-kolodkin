b226af548bb2b7207e8ed5fba44bd7b82cac8adf
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2023-12-27 18:18:23 +0300

    Задача 3. Файловый сервер - запись файла

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -10,8 +12,79 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  function tryToUploadFile() {
+    const limitStream = new LimitSizeStream({limit: 1024 * 1024});
+    const stream = fs.createWriteStream(filepath, {
+      flags: 'wx',
+    });
+
+    req.pipe(limitStream).pipe(stream);
+
+    req.on('data', (payload) => {
+      if (!payload || payload.toString() === '') {
+        res.statusCode = 400;
+        res.end('Bad request');
+      }
+    });
+
+    stream.on('error', (err) => {
+      console.warn(err);
+      if (err.code === 'EEXIST') {
+        res.statusCode = 409;
+        res.end(`File ${pathname} already exists`);
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
+    });
+
+    limitStream.on('error', (err) => {
+      console.warn(err);
+      if (err.code === 'LIMIT_EXCEEDED') {
+        res.statusCode = 413;
+        res.end('File is too large');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal server error');
+      }
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          console.warn(err);
+        }
+      });
+    });
+
+    stream.on('finish', () => {
+      res.statusCode = 201;
+      res.end(`Finish. File ${pathname} uploaded`);
+    });
+
+    req.on('aborted', () => {
+      limitStream.destroy();
+      stream.destroy();
+
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          console.warn(err);
+        }
+      });
+    });
+
+    res.on('close', () => {
+      limitStream.destroy();
+      stream.destroy();
+    });
+  }
+
   switch (req.method) {
     case 'POST':
+
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Bad Request');
+      } else {
+        tryToUploadFile();
+      }
 
       break;
 
