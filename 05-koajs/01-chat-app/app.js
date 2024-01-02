5029e4ef357a85c090c8133c7f817479afaeadc8
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2024-01-02 22:15:57 +0300

    Задача 5. Чат на Koa.js

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,41 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let clients = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  console.log('subscribe');
+
+  let promiseResolveFunc = null;
+
+  ctx.body = await (new Promise((resolve) => {
+    promiseResolveFunc = resolve;
+    clients.push(promiseResolveFunc);
+  }));
+
+  ctx.req.on('close', () => {
+    const start = clients.indexOf(promiseResolveFunc);
+
+    if (promiseResolveFunc !== null && start >= 0) {
+      clients.splice(start, 1);
+    }
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  console.log('publish');
+  console.log(ctx.request.body);
+
+  const message = ctx.request.body.message;
+  if (message) {
+    clients.forEach(function(promiseResolveFunc) {
+      promiseResolveFunc(message);
+    });
+  }
+
+  ctx.body = 0;
+
+  await next();
 });
 
 app.use(router.routes());
