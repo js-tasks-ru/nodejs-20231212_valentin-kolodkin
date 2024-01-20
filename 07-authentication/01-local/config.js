c94fe1728ad3166df52e70e23ca3f93e3d6d0de6
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2024-01-20 19:36:25 +0300

    Задача 7. Настройка локальной стратегии

--- 07-authentication/01-local/config.js
+++ 07-authentication/01-local/config.js
@@ -2,7 +2,7 @@ module.exports = {
   mongodb: {
     uri: (process.env.NODE_ENV === 'test') ?
       'mongodb://127.0.0.1:27017/7-module-1-task' :
-      'mongodb://127.0.0.1:27017/any-shop',
+      'mongodb://127.0.0.1:27017/AnyShop',
   },
   crypto: {
     iterations: (process.env.NODE_ENV === 'test' ? 1 : 12000),
