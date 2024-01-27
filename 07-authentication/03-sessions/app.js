5f350f25e047d274d90f4c444324e8edebf63176
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2024-01-27 20:43:45 +0300

    Задача 7. Работа с пользователем, сессии

--- 07-authentication/03-sessions/app.js
+++ 07-authentication/03-sessions/app.js
@@ -33,6 +33,12 @@ app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
 
+    await Session.create({
+      token: token,
+      lastVisit: new Date(),
+      user: user._id,
+    });
+
     return token;
   };
 
@@ -43,7 +49,20 @@ const router = new Router({prefix: '/api'});
 
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
-  if (!header) return next();
+  if (!header) {
+    return next();
+  }
+
+  const explodedHeader = header.split(' ');
+  if (explodedHeader.length < 2) {
+    return next();
+  }
+
+  await Session.findOneAndUpdate({
+    token: explodedHeader[1],
+  }, {
+    lastVisit: new Date(),
+  });
 
   return next();
 });
@@ -53,7 +72,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
--- 07-authentication/03-sessions/libs/mustBeAuthenticated.js
+++ 07-authentication/03-sessions/libs/mustBeAuthenticated.js
@@ -1,3 +1,26 @@
-module.exports = function mustBeAuthenticated(ctx, next) {
+const Session = require('../models/Session');
+
+module.exports = async function mustBeAuthenticated(ctx, next) {
+  const header = ctx.request.get('Authorization');
+
+  if (!header) {
+    ctx.throw(401, 'Пользователь не залогинен');
+  }
+
+  const explodedHeader = header.split(' ');
+  if (explodedHeader.length < 2) {
+    ctx.throw(400, 'Bad request');
+  }
+
+  const session = await Session.findOne({
+    token: explodedHeader[1],
+  }).populate('user');
+
+  if (!session) {
+    ctx.throw(401, 'Неверный аутентификационный токен');
+  }
+
+  ctx.user = session.user;
+
   return next();
 };
