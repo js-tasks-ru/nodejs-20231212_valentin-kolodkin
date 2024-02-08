9c29ca0f0d9c1bdbbeb2646bcd734820e83cc1b2
Valentin Kolodkin <kolodkinvalentin@mail.ru>
2024-02-08 19:21:48 +0300

    Чат с использованием Websockets

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -7,11 +7,36 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const token = socket.handshake.query.token;
+    if (!token) {
+      const err = new Error('anonymous sessions are not allowed');
+      next(err);
+    }
+
+    const session = await Session.findOne({token});
+    if (!session) {
+      const err = new Error('wrong or expired session token');
+      next(err);
+    }
+
+    socket.user = session.user;
+
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const user = socket.user;
+
+      const messageObj = new Message({
+        date: new Date(),
+        text: msg,
+        chat: user._id,
+        user: user.displayName,
+      });
+
+      await messageObj.save();
+    });
   });
 
   return io;
